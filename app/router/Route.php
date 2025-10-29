304f18e4483ceee72492626179cd95b0c6c3dc6d
edimar bino junior <edimarbinojr21@gmail.com>
2025-10-28 21:10:15 -0400

    28/10
    28/10

--- app/router/Route.php
+++ app/router/Route.php
@@ -1,32 +1,66 @@
 <?php
 
-namespace App\router;
+namespace app\router;
+
+use app\controller\cliente\InsertCliente;
 
 class Route
 {
-    public static function router()
+    #Metodo GET para carregar paginas.
+    private function get()
     {
-        $self = new self();
-        $base_dir = __DIR__ . '/../view';
-
+        #Diretorio da camada de visualização.
+        $base_dir =  __DIR__ . '/../view';
+        #Captura o caminha completo até o arquivo.
         $path = $base_dir . $_SERVER['REQUEST_URI'] . '.php';
-
-        if ($_SERVER['REQUEST_URI'] == '/') {
-            $path = $base_dir . '/home.php';
-            echo 'Adicionar pagina inicial da aplicação';
+        if ($_SERVER['REQUEST_URI'] === '/home') {
+            require_once $base_dir . '/home.php';
             die;
         }
         if ($_SERVER['REQUEST_URI'] === '/') {
-            echo 'Adicionar pagina inicial da aplicação';
+            require_once $base_dir . '/home.php';
             die;
         }
-
-        #caso o arquivo nao exista iremos adiconar erro 404
+        #Caso o arquivo não exista adicionar o erro 404.
         if (!file_exists($path)) {
             require_once $base_dir . '/error/404.php';
             die;
         }
         require_once $path;
         die;
+    }
+    private function post()
+    {
+        $request_uri = $_SERVER["REQUEST_URI"];
+        $request = explode('/', $request_uri);
+        $option = $request[1];
+        $resource = $request[2];
+        switch ($option) {
+            case 'cliente':
+                if ($resource === 'insert') {
+                    InsertCliente::Insert();
+                }
+                if ($resource === 'update') {
+                }
+                if ($resource === 'delete') {
+                }
+                break;
+
+            default:
+                # code...
+                break;
+        }
+        die;
+    }
+    public static function router()
+    {
+        $self = new self;
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method === 'GET') {
+            $self->get();
+        }
+        if ($method === 'POST') {
+            $self->post();
+        }
     }
 }
--- app/view/cliente/index.php
+++ app/view/cliente/index.php
@@ -8,7 +8,6 @@
     <title>Lista cliente</title>
 </head>
 
-
 <body>
 
     <form id="form">
@@ -22,49 +21,118 @@
                         </h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+
                     <div class="modal-body">
                         <div class="row">
-                            <div class="col-6 mt-3">
-                                <div class="form-floating mb-3">
-                                    <input type="search" class="form-control fs" id="nome" name="nome" placeholder="nome">
-                                    <label for="nome"><i class="fa-solid fa-magnifying-glass"></i>
-                                        Digite seu Nome
-                                    </label>
+                            <!-- Nome -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
+                                    <label for="nome">Digite seu nome</label>
                                 </div>
                             </div>
-                            <div class="col-6 mt-3">
-                                <div class="form-floating mb-3">
-                                    <input type="search" class="form-control fs" id="sobrenome" name="sobrenome" placeholder="sobrenome">
-                                    <label for="sobrenome"><i class="fa-solid fa-magnifying-glass"></i>
-                                        Digite seu sobrenome
-                                    </label>
+
+                            <!-- Sobrenome -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="sobre_nome" name="sobre_nome" placeholder="Digite seu sobre nome">
+                                    <label for="sobre_nome">Digite seu sobrenome</label>
                                 </div>
                             </div>
-                            <div class="col-6 mt-3">
-                                <div class="form-floating mb-3">
-                                    <input type="search" class="form-control fs" id="cpf" name="cpf" placeholder="cpf">
-                                    <label for="cpf"><i class="fa-solid fa-magnifying-glass"></i>
-                                        Digite seu CPF
-                                    </label>
+
+                            <!-- CPF -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF">
+                                    <label for="cpf">Digite seu CPF</label>
                                 </div>
                             </div>
-                            <div class="col-6 mt-3">
-                                <div class="form-floating mb-3">
-                                    <input type="search" class="form-control fs" id="rg" name="rg" placeholder="rg">
-                                    <label for="rg"><i class="fa-solid fa-magnifying-glass"></i>
-                                        Digite seu RG
-                                    </label>
+
+                            <!-- CEP -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cep" name="cep" placeholder="Digite seu CEP">
+                                    <label for="cep">Digite seu CEP</label>
+                                </div>
+                            </div>
+
+                            <!-- Número -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="numero" name="numero" placeholder="Digite número do imóvel">
+                                    <label for="numero">Digite número do imóvel</label>
+                                </div>
+                            </div>
+
+                            <!-- Logradouro -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="logradouro" name="logradouro" placeholder="Digite número do imóvel">
+                                    <label for="logradouro">Digite o logradouro</label>
+                                </div>
+                            </div>
+
+                            <!-- Bairro -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="bairro" name="bairro" placeholder="Digite o nome do bairro">
+                                    <label for="bairro">Digite o nome do bairro</label>
+                                </div>
+                            </div>
+
+                            <!-- Cidade -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Digite o nome da cidade">
+                                    <label for="cidade">Digite o nome da cidade</label>
+                                </div>
+                            </div>
+
+                            <!-- UF -->
+                            <div class="col-md-6 mt-3">
+                                <div class="form-floating">
+                                    <select class="form-select" id="uf" name="uf" aria-label="Selecione o estado">
+                                        <option value="">Selecione...</option>
+                                        <option value="AC">Acre</option>
+                                        <option value="AL">Alagoas</option>
+                                        <option value="AP">Amapá</option>
+                                        <option value="AM">Amazonas</option>
+                                        <option value="BA">Bahia</option>
+                                        <option value="CE">Ceará</option>
+                                        <option value="DF">Distrito Federal</option>
+                                        <option value="ES">Espírito Santo</option>
+                                        <option value="GO">Goiás</option>
+                                        <option value="MA">Maranhão</option>
+                                        <option value="MT">Mato Grosso</option>
+                                        <option value="MS">Mato Grosso do Sul</option>
+                                        <option value="MG">Minas Gerais</option>
+                                        <option value="PA">Pará</option>
+                                        <option value="PB">Paraíba</option>
+                                        <option value="PR">Paraná</option>
+                                        <option value="PE">Pernambuco</option>
+                                        <option value="PI">Piauí</option>
+                                        <option value="RJ">Rio de Janeiro</option>
+                                        <option value="RN">Rio Grande do Norte</option>
+                                        <option value="RS">Rio Grande do Sul</option>
+                                        <option value="RO">Rondônia</option>
+                                        <option value="RR">Roraima</option>
+                                        <option value="SC">Santa Catarina</option>
+                                        <option value="SP">São Paulo</option>
+                                        <option value="SE">Sergipe</option>
+                                        <option value="TO">Tocantins</option>
+                                        <option value="EX">Estrangeiro</option>
+                                    </select>
+                                    <label for="uf">Selecione o Estado</label>
                                 </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
+
+                        <div class="modal-footer mt-4">
                             <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                                <i class="fa-solid fa-ban"></i>
-                                Cancelar
+                                <i class="fa-solid fa-ban"></i> Cancelar
                             </button>
-                            <button id="salvarRegistro" type="button" class="btn btn-success">
-                                <i class="fa-solid fa-floppy-disk"></i>
-                                Salvar
+                            <button id="salvar" type="button" class="btn btn-success">
+                                <i class="fa-solid fa-floppy-disk"></i> Salvar
                             </button>
                         </div>
                     </div>
@@ -75,43 +143,32 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 mt-3 mb-3">
-                    <button type="button" onclick="" class="btn btn-primary btn-sm btn-success" data-bs-toggle="modal"
-                        data-bs-target="#cadastroCliente"><i class="fa-solid fa-user-plus"></i>
-                        Novo Cliente
+                    <button type="button" class="btn btn-primary btn-sm btn-success" data-bs-toggle="modal"
+                        data-bs-target="#cadastroCliente">
+                        <i class="fa-solid fa-user-plus"></i> Novo Cliente
                     </button>
                 </div>
+
                 <div class="col-12">
-                    <h1><i class="fa-regular fa-user"></i>
-                        Clientes Cadastrados
-                    </h1>
+                    <h1><i class="fa-regular fa-user"></i> Clientes Cadastrados</h1>
                     <div class="col-6 mt-3">
                         <div class="form-floating mb-3">
                             <input type="search" class="form-control fs" id="pesquisa" name="pesquisa" placeholder="Pesquisar">
-                            <label for="pesquisa"><i class="fa-solid fa-magnifying-glass"></i>
-                                Digite seu Nome
-                            </label>
+                            <label for="pesquisa"><i class="fa-solid fa-magnifying-glass"></i> Digite seu Nome</label>
                         </div>
                     </div>
+
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th><i class="fa-solid fa-fingerprint"></i>Código</th>
-                                <th><i class="fa-solid fa-user"></i>Nome</th>
-                                <th><i class="fa-solid fa-id-card"></i>Sobrenome</th>
-                                <th><i class="fa-solid fa-id-card"></i>CPF</th>
-                                <th><i class="fa-solid fa-id-card"></i>RG</th>
-
+                                <th><i class="fa-solid fa-fingerprint"></i> Código</th>
+                                <th><i class="fa-solid fa-user"></i> Nome</th>
+                                <th><i class="fa-solid fa-id-card"></i> Sobrenome</th>
+                                <th><i class="fa-solid fa-id-card"></i> CPF</th>
+                                <th><i class="fa-solid fa-id-card"></i> RG</th>
                             </tr>
                         </thead>
                         <tbody id="listacliente">
-                            <tr>
-                                <th>Código</th>
-                                <th>Nome</th>
-                                <th>Sobrenome</th>
-                                <th>CPF</th>
-                                <th>RG</th>
-                            </tr>
-
                             <tr>
                                 <td>001</td>
                                 <td>Lucas</td>
@@ -119,7 +176,6 @@
                                 <td>123.456.789-00</td>
                                 <td>12.345.678-9</td>
                             </tr>
-
                             <tr>
                                 <td>002</td>
                                 <td>Mariana</td>
@@ -127,7 +183,6 @@
                                 <td>987.654.321-00</td>
                                 <td>98.765.432-1</td>
                             </tr>
-
                             <tr>
                                 <td>003</td>
                                 <td>Felipe</td>
@@ -135,7 +190,6 @@
                                 <td>456.789.123-00</td>
                                 <td>45.678.912-3</td>
                             </tr>
-
                             <tr>
                                 <td>004</td>
                                 <td>Ana</td>
@@ -143,7 +197,6 @@
                                 <td>321.654.987-00</td>
                                 <td>32.165.498-7</td>
                             </tr>
-
                             <tr>
                                 <td>005</td>
                                 <td>João</td>
@@ -152,8 +205,6 @@
                                 <td>15.975.348-6</td>
                             </tr>
                         </tbody>
-
-                        </tbody>
                     </table>
                 </div>
             </div>
@@ -161,6 +212,7 @@
     </form>
 
     <?php include_once '/usr/share/nginx/app/view/script.php' ?>
+    <script defer type="module" src="/js/listacliente.js"></script>
 
 </body>
 
